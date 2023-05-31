df48bff379764a85873fb4086695ba5dcd9df448
Kaiquii <112667885+Kaiquii@users.noreply.github.com>
2023-05-30 23:03:33 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,96 @@
+const modal = document.querySelector('.modal-container')
+const tbody = document.querySelector('tbody')
+const sProduto = document.querySelector('#m-nome')
+const sCategoria = document.querySelector('#m-funcao')
+const sValor = document.querySelector('#m-salario')
+const btnSalvar = document.querySelector('#btnSalvar')
+
+let itens
+let id
+
+function openModal(edit = false, index = 0) {
+  modal.classList.add('active')
+
+  modal.onclick = e => {
+    if (e.target.className.indexOf('modal-container') !== -1) {
+      modal.classList.remove('active')
+    }
+  }
+
+  if (edit) {
+    sProduto.value = itens[index].produto
+    sCategoria.value = itens[index].categoria
+    sValor.value = itens[index].valor
+    id = index
+  } else {
+    sProduto.value = ''
+    sCategoria.value = ''
+    sValor.value = ''
+  }
+  
+}
+
+function editItem(index) {
+
+  openModal(true, index)
+}
+
+function deleteItem(index) {
+  itens.splice(index, 1)
+  setItensBD()
+  loadItens()
+}
+
+function insertItem(item, index) {
+  let tr = document.createElement('tr')
+
+  tr.innerHTML = `
+    <td>${item.produto}</td>
+    <td>${item.categoria}</td>
+    <td>R$ ${item.valor}</td>
+    <td class="acao">
+      <button onclick="editItem(${index})"><i class='bx bx-edit' ></i></button>
+    </td>
+    <td class="acao">
+      <button onclick="deleteItem(${index})"><i class='bx bx-trash'></i></button>
+    </td>
+  `
+  tbody.appendChild(tr)
+}
+
+btnSalvar.onclick = e => {
+  
+  if (sProduto.value == '' || sCategoria.value == '' || sValor.value == '') {
+    return
+  }
+
+  e.preventDefault();
+
+  if (id !== undefined) {
+    itens[id].produto = sProduto.value
+    itens[id].categoria = sCategoria.value
+    itens[id].valor = sValor.value
+  } else {
+    itens.push({'produto': sProduto.value, 'categoria': sCategoria.value, 'valor': sValor.value})
+  }
+
+  setItensBD()
+
+  modal.classList.remove('active')
+  loadItens()
+  id = undefined
+}
+
+function loadItens() {
+  itens = getItensBD()
+  tbody.innerHTML = ''
+  itens.forEach((item, index) => {
+    insertItem(item, index)
+  })
+
+}
+
+const getItensBD = () => JSON.parse(localStorage.getItem('dbfunc')) ?? []
+const setItensBD = () => localStorage.setItem('dbfunc', JSON.stringify(itens))
+
+loadItens()
